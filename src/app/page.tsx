5255ff9be427406082a3dcc8883d8a5868e5a837
Johnny Gérard <john@jgerard.dev>
2025-04-02 17:38:19 +0200

    feat: add ArticleHeader to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { ArticleHeader } from "@/component/article-header";
 import { memo } from "react";
 
 const HomePage = () => {
-  return null;
+  return (
+    <main className="py-12">
+      <article>
+        <ArticleHeader
+          date="2025-04-02"
+          tags={["Nature", "Ocean", "Exploration"]}
+          title="Exploring the Depths of the Ocean"
+        />
+      </article>
+    </main>
+  );
 };
 
 export default memo(HomePage);
