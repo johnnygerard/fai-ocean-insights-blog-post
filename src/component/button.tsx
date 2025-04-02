3d450c518936cdbf1d9384f6e7f106247bd17cbf
Johnny Gérard <john@jgerard.dev>
2025-04-02 17:14:01 +0200

    refactor: extract color variable

--- src/component/button.tsx
+++ src/component/button.tsx
@@ -18,7 +18,7 @@ export const Button = memo(
           "text-[0.9375rem]/[1.25rem] font-medium",
           {
             text: "",
-            primary: "rounded-lg bg-[#FF5733] px-4 py-2 text-[#050100]",
+            primary: "rounded-lg bg-accent px-4 py-2 text-[#050100]",
           }[variant],
           className,
         )}
