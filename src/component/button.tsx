66eb58a874ea2979021b7f0c42fab9db9cfaf6f7
Johnny Gérard <john@jgerard.dev>
2025-04-02 14:47:33 +0200

    feat: add variant prop to button.tsx

--- src/component/button.tsx
+++ src/component/button.tsx
@@ -2,24 +2,31 @@ import { cn } from "@/util/cn";
 import type { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
 import { memo } from "react";
 
-type Props = DetailedHTMLProps<
+type ButtonProps = DetailedHTMLProps<
   ButtonHTMLAttributes<HTMLButtonElement>,
   HTMLButtonElement
 >;
 
-export const Button = memo(({ children, className, ...props }: Props) => {
-  return (
-    <button
-      className={cn(
-        "text-[0.9375rem]/[1.25rem] font-medium text-[#050100]",
-        "rounded-lg bg-[#FF5733] px-4 py-2",
-        className,
-      )}
-      {...props}
-    >
-      {children}
-    </button>
-  );
-});
+type Props = ButtonProps & { variant: "primary" | "text" };
+
+export const Button = memo(
+  ({ children, className, variant, ...props }: Props) => {
+    return (
+      <button
+        className={cn(
+          "text-[0.9375rem]/[1.25rem] font-medium",
+          {
+            text: "text-[#E4DAD7]",
+            primary: "rounded-lg bg-[#FF5733] px-4 py-2 text-[#050100]",
+          }[variant],
+          className,
+        )}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  },
+);
 
 Button.displayName = "Button";
