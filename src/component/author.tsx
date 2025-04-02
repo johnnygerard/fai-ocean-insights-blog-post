4427ba41b52fabb5b1ef462811b7df61d97e9106
Johnny Gérard <john@jgerard.dev>
2025-04-02 17:10:17 +0200

    feat: add author.tsx

---
+++ src/component/author.tsx
@@ -0,0 +1,30 @@
+import { cn } from "@/util/cn";
+import Image from "next/image";
+import { memo } from "react";
+
+type Props = {
+  className?: string;
+  avatarUrl: string;
+  name: string;
+  title: string;
+};
+
+export const Author = memo(({ avatarUrl, className, name, title }: Props) => {
+  return (
+    <div className={cn("flex items-center gap-3", className)}>
+      <Image
+        className="size-9"
+        src={avatarUrl}
+        alt=""
+        width={1024}
+        height={1024}
+      />
+      <div className="flex w-60 flex-col text-[0.8125rem]/[1rem]">
+        <span className="truncate font-semibold">{name}</span>
+        <span className="truncate text-dim">{title}</span>
+      </div>
+    </div>
+  );
+});
+
+Author.displayName = "Author";
