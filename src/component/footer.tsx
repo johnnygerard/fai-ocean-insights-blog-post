61b140fb58b6eacbdb0b7813facd77354791085c
Johnny Gérard <john@jgerard.dev>
2025-04-03 23:14:00 +0200

    feat: footer content within nav element

--- src/component/footer.tsx
+++ src/component/footer.tsx
@@ -10,68 +10,64 @@ type Props = {
 
 export const Footer = memo(({ className }: Props) => {
   return (
-    <footer
-      className={cn(
-        "border-t border-[#B6979133] p-12 pt-6",
-        "flex gap-12",
-        className,
-      )}
-    >
-      <div className="flex flex-1 flex-col justify-between">
-        <Logo />
-        <SocialMediaLinks />
-      </div>
-      <NavigationList
-        header="Explore"
-        links={[
-          {
-            href: "/explore/deep-sea",
-            text: "Deep Sea",
-          },
-          {
-            href: "/explore/marine-life",
-            text: "Marine Life",
-          },
-          {
-            href: "/explore/coral-reefs",
-            text: "Coral Reefs",
-          },
-        ]}
-      />
-      <NavigationList
-        header="Conservation"
-        links={[
-          {
-            href: "/conservation/initiatives",
-            text: "Initiatives",
-          },
-          {
-            href: "/conservation/research",
-            text: "Research",
-          },
-          {
-            href: "/conservation/get-involved",
-            text: "Get Involved",
-          },
-        ]}
-      />
-      <NavigationList
-        header="About Us"
-        links={[
-          {
-            href: "/about/our-mission",
-            text: "Our Mission",
-          },
-          {
-            href: "/about/team",
-            text: "Team",
-          },
-          {
-            href: "/about/contact",
-            text: "Contact",
-          },
-        ]}
-      />
+    <footer className={cn("border-t border-[#B6979133] p-12 pt-6", className)}>
+      <nav className="flex gap-12">
+        <div className="flex flex-1 flex-col justify-between">
+          <Logo />
+          <SocialMediaLinks />
+        </div>
+        <NavigationList
+          header="Explore"
+          links={[
+            {
+              href: "/explore/deep-sea",
+              text: "Deep Sea",
+            },
+            {
+              href: "/explore/marine-life",
+              text: "Marine Life",
+            },
+            {
+              href: "/explore/coral-reefs",
+              text: "Coral Reefs",
+            },
+          ]}
+        />
+        <NavigationList
+          header="Conservation"
+          links={[
+            {
+              href: "/conservation/initiatives",
+              text: "Initiatives",
+            },
+            {
+              href: "/conservation/research",
+              text: "Research",
+            },
+            {
+              href: "/conservation/get-involved",
+              text: "Get Involved",
+            },
+          ]}
+        />
+        <NavigationList
+          header="About Us"
+          links={[
+            {
+              href: "/about/our-mission",
+              text: "Our Mission",
+            },
+            {
+              href: "/about/team",
+              text: "Team",
+            },
+            {
+              href: "/about/contact",
+              text: "Contact",
+            },
+          ]}
+        />
+      </nav>
     </footer>
   );
 });
