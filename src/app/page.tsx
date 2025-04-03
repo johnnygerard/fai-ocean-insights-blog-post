f96995abc4827744e887ab46712b382ab8127b37
Johnny Gérard <john@jgerard.dev>
2025-04-03 13:25:03 +0200

    feat: use fixed height for image containers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,16 +23,16 @@ const HomePage = async () => {
           title="Exploring the Depths of the Ocean"
         />
         <Image
+          className="my-6 h-220 w-full object-cover"
           src={hero}
           alt="A vibrant coral reef teeming with colorful fish and marine life under clear blue water."
           priority
-          className="my-6 w-full"
         />
         <ArticleSection {...articleSections[0]} />
         <div className="my-6 px-12">
           <video
             src="/asset/video/coral-reefs.mp4"
-            className="w-full rounded-2xl"
+            className="h-190 w-full rounded-2xl object-cover"
             width={1312}
             height={720}
             autoPlay
@@ -47,7 +47,7 @@ const HomePage = async () => {
           <Image
             src={hydrothermalVent}
             alt="A vibrant underwater scene showcasing a hydrothermal vent surrounded by colorful tube worms and various fish species swimming nearby."
-            className="w-full rounded-lg"
+            className="h-156 w-full rounded-lg object-cover"
           />
         </div>
       </article>
