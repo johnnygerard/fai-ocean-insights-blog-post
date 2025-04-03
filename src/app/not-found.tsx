ec58aa50d56d79b094c3b9f5f20b79d4e310be34
Johnny Gérard <john@jgerard.dev>
2025-04-04 00:15:42 +0200

    feat: style not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/util/cn";
 import { Metadata } from "next";
 import Link from "next/link";
 import { memo } from "react";
@@ -10,9 +11,21 @@ const NotFound = () => {
   return (
     <main className="grid flex-1 place-items-center">
       <div className="text-center">
-        <h1>404 Not Found</h1>
-        <p>Sorry, the page you are looking for does not exist.</p>
-        <Link href="/">Back to Home</Link>
+        <h1 className="mb-4 font-display text-[3rem] font-bold">
+          404 Not Found
+        </h1>
+        <p className="mb-6 text-[1.0625rem]/[1.75rem]">
+          Sorry, the page you are looking for does not exist.
+        </p>
+        <Link
+          href="/"
+          className={cn(
+            "text-[1.0625rem]/[1.75rem] font-medium text-accent",
+            "transition-[opacity] hover:opacity-80",
+          )}
+        >
+          Back to Home
+        </Link>
       </div>
     </main>
   );
