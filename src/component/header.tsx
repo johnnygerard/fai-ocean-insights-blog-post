a92e9d8694fd38b75dde246257b044180b5c2fb1
Johnny Gérard <john@jgerard.dev>
2025-04-04 22:18:52 +0200

    feat: add header-menu.tsx to header

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/component/button";
+import { HeaderMenu } from "@/component/header-menu";
 import { Logo } from "@/component/logo";
 import { VerticalRule } from "@/component/svg/vertical-rule";
 import { cn } from "@/util/cn";
@@ -11,15 +12,19 @@ type Props = {
 export const Header = memo(({ className }: Props) => {
   return (
     <header
-      className={cn("flex items-center justify-between px-12 py-4", className)}
+      className={cn(
+        "top-0 z-10 bg-canvas/90 max-md:sticky",
+        "flex items-center justify-between px-4 py-4 sm:px-8 md:px-12",
+        className,
+      )}
     >
-      <Logo />
-      <div className="flex items-center gap-8">
+      <Logo className="z-20 max-md:w-40" />
+      <HeaderMenu>
         <Button variant="text">Download app</Button>
-        <VerticalRule />
+        <VerticalRule className="max-md:hidden" />
         <Button variant="text">Log in</Button>
         <Button variant="primary">Try it free</Button>
-      </div>
+      </HeaderMenu>
     </header>
   );
 });
