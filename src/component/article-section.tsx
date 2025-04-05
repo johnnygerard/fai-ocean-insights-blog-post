f004468c1e7e9433dbd5e0c46fd298c72429d0a6
Johnny Gérard <john@jgerard.dev>
2025-04-05 13:45:30 +0200

    feat: make the article-section.tsx responsive

--- src/component/article-section.tsx
+++ src/component/article-section.tsx
@@ -9,11 +9,30 @@ type Props = {
 
 export const ArticleSection = memo(({ className, heading, content }: Props) => {
   return (
-    <section className={cn("mx-auto w-180 py-12", className)}>
-      <h2 className="font-display text-[1.75rem]/[2rem] font-bold -tracking-[0.035rem]">
+    <section
+      className={cn(
+        "tw_body_px mx-auto w-full max-w-180",
+        "py-6 sm:py-8 md:py-10 lg:py-12",
+        className,
+      )}
+    >
+      <h2
+        className={cn(
+          "font-display font-bold -tracking-[0.025rem] lg:-tracking-[0.035rem]",
+          "text-[1.25rem]/[1.5rem] sm:text-[1.5rem]/[1.75rem]",
+          "md:text-[1.625rem]/[1.875rem] lg:text-[1.75rem]/[2rem]",
+        )}
+      >
         {heading}
       </h2>
-      <p className="mt-6 text-[1.0625rem]/[1.75rem]">{content}</p>
+      <p
+        className={cn(
+          "mt-4 text-[0.9375rem]/[1.5rem] sm:mt-5 md:mt-6",
+          "sm:text-[1rem]/[1.625rem] md:text-[1.0625rem]/[1.75rem]",
+        )}
+      >
+        {content}
+      </p>
     </section>
   );
 });
