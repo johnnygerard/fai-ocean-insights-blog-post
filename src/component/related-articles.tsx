1d91348e98bbd31bcc80ec3756666ca0be76cb44
Johnny Gérard <john@jgerard.dev>
2025-04-05 12:45:47 +0200

    feat: make related-articles.tsx layout responsive

--- src/component/related-articles.tsx
+++ src/component/related-articles.tsx
@@ -10,19 +10,24 @@ type Props = {
 
 export const RelatedArticles = memo(({ articles, className }: Props) => {
   return (
-    <aside className={cn("px-12 py-8", className)}>
+    <aside
+      className={cn(
+        "px-4 py-6 sm:px-6 md:px-8 md:py-7 lg:px-12 lg:py-8",
+        className,
+      )}
+    >
       <nav aria-label="Related Articles">
         <h2
           className={cn(
             "border-t-[1.5px] border-[#B6979133]",
-            "py-4 font-display text-[1.75rem]/[2rem] font-bold -tracking-[0.035rem]",
+            "py-3 font-display text-[1.75rem]/[2rem] font-bold -tracking-[0.035rem] md:py-4",
           )}
         >
           Related Articles
         </h2>
-        <ul className="flex gap-6">
+        <ul className="grid gap-8 md:grid-cols-2 md:gap-6 xl:grid-cols-3">
           {articles.map((article) => (
-            <li className="flex-1" key={article.title}>
+            <li key={article.title}>
               <ArticlePreview article={article} />
             </li>
           ))}
