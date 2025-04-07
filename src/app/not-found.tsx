1a1be37eb66ebca58014f3d43620d2cb8a077db4
Johnny Gérard <john@jgerard.dev>
2025-04-07 17:18:49 +0200

    feat: improve not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,3 @@
-import { clsx } from "clsx";
 import { Metadata } from "next";
 import Link from "next/link";
 import { memo } from "react";
@@ -14,13 +13,19 @@ const NotFound = () => {
         <h1 className="mb-4 font-display text-h1 font-bold">404 Not Found</h1>
         <p className="mb-6">
           Sorry, the page you are looking for does not exist.
+          <br />
+          This single-page demo website is part of my{" "}
+          <a
+            href="https://www.jgerard.dev/"
+            className="font-medium text-accent transition-[opacity] hover:opacity-80"
+          >
+            portfolio
+          </a>
+          .
         </p>
         <Link
           href="/"
-          className={clsx(
-            "font-medium text-accent",
-            "transition-[opacity] hover:opacity-80",
-          )}
+          className="font-medium text-accent transition-[opacity] hover:opacity-80"
         >
           Back to Home
         </Link>
