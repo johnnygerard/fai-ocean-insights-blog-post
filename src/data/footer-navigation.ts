4f7b86ca3a73db2769b45ea422a17d6cd69ccf20
Johnny Gérard <john@jgerard.dev>
2025-12-12 23:25:52 +0100

    feat: add src/data/footer-navigation.ts

---
+++ src/data/footer-navigation.ts
@@ -0,0 +1,61 @@
+type footerNavigationLink = {
+  href: string;
+  text: string;
+};
+
+export const footerNavigation: Array<{
+  header: string;
+  links: footerNavigationLink[];
+}> = [
+  {
+    header: "Explore",
+    links: [
+      {
+        href: "/explore/deep-sea",
+        text: "Deep Sea",
+      },
+      {
+        href: "/explore/marine-life",
+        text: "Marine Life",
+      },
+      {
+        href: "/explore/coral-reefs",
+        text: "Coral Reefs",
+      },
+    ],
+  },
+  {
+    header: "Conservation",
+    links: [
+      {
+        href: "/conservation/initiatives",
+        text: "Initiatives",
+      },
+      {
+        href: "/conservation/research",
+        text: "Research",
+      },
+      {
+        href: "/conservation/get-involved",
+        text: "Get Involved",
+      },
+    ],
+  },
+  {
+    header: "About Us",
+    links: [
+      {
+        href: "/about/our-mission",
+        text: "Our Mission",
+      },
+      {
+        href: "/about/team",
+        text: "Team",
+      },
+      {
+        href: "/about/contact",
+        text: "Contact",
+      },
+    ],
+  },
+];
