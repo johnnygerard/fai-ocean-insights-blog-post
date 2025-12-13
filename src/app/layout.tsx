ad2cd21f98ede282e4cbcea2ff49eabcd7337f58
Johnny Gérard <john@jgerard.dev>
2025-12-13 01:24:45 +0100

    feat: implement root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,31 @@
+import { clsx } from "clsx";
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
+import { Domine, Outfit } from "next/font/google";
 import type { FC, ReactNode } from "react";
+import { Footer } from "~/components/footer";
+import { Header } from "~/components/header";
+import { ScrollToTop } from "~/components/scroll-to-top";
 import "./globals.css";
 
-const geistSans = Geist({
+const outfit = Outfit({
   display: "swap",
   subsets: ["latin"],
-  variable: "--font-geist-sans",
+  variable: "--font-outfit",
 });
 
-const APP_NAME = "appName";
-const TITLE = "title";
-const DESCRIPTION = "description";
+const domine = Domine({
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--font-domine",
+});
+
+const APP_NAME = "OceanInsights";
+const TITLE = "Exploring the Depths of the Ocean";
+const DESCRIPTION =
+  "A fascinating journey into the mysteries of the ocean, uncovering the unknown and the beauty beneath the waves.";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://placeholder.example"),
+  metadataBase: new URL("https://oceaninsights.jgerard.dev"),
   title: {
     template: `%s | ${APP_NAME}`,
     default: TITLE,
@@ -36,11 +47,21 @@ type Props = {
 const RootLayout: FC<Props> = ({ children }) => {
   return (
     <html
-      className={geistSans.variable}
+      className={clsx(outfit.variable, domine.variable)}
       data-scroll-behavior="smooth"
       lang="en-US"
     >
-      <body className="font-sans">{children}</body>
+      <body
+        className={clsx(
+          "mx-auto max-w-480 bg-canvas font-sans text-base text-[#E4DAD7]",
+          "flex min-h-screen flex-col",
+        )}
+      >
+        <Header />
+        {children}
+        <Footer className="mt-24" />
+        <ScrollToTop />
+      </body>
     </html>
   );
 };
