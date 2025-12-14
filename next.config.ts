397f3a5350486fd159e836632563cc250ca86285
Johnny Gérard <john@jgerard.dev>
2025-12-14 03:18:45 +0100

    perf: configure deviceSizes in next.config.js

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    // Note that 1408 is the largest image width used in the app
+    deviceSizes: [480, 640, 800, 1280, 1408],
     loader: "custom",
     loaderFile: "src/image-loader.ts",
   },
